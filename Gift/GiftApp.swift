0971f4c3390a49938f4cb388d7a293206554c2c1
Vladimir Cezar <vladimircezar@icloud.com>
2024-10-29 19:30:13 -0400

    Initial Commit

---
+++ Gift/GiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftApp.swift
+//  Gift
+//
+//  Created by Vladimir Cezar on 2024-10-29.
+//
+
+import SwiftUI
+
+@main
+struct GiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
